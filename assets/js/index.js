6c15664b4e9c9e33e3a2aae3a17496e5792733fc
Nada Zirari <zirarinada20@gmail.com>
2024-12-03 23:26:58 +0100

    recuperrer les donnees necessaires par id et les stockées dans des variables

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const see_all_players_section = document.getElementById("all-players-section")
+const filtered_players_section = document.getElementById("players-filtered");
+
+const FormAll=document.getElementById('formumaireAll')
+const see_all_players_button = document.getElementById("all-players")
+const see_all_players = document.querySelector(".see-all-players")
+const add_player_button = document.getElementById("add-player")
+const add_player_form = document.getElementById("add-player-form")
+const close_form_button = document.getElementById("close-form")
+const form_layover = document.querySelector(".layover")
+const form = document.getElementById("form")
+const save_button_form = document.getElementById("save-player-button")
+const clear_player_card = document.getElementById("empty-card")
+const clear_all_cards_button = document.getElementById("clear-cards");
